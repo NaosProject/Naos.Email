6f9b26e437a26e665be61a8dd68bcc73a55a6013
Suraj Gupta <SurajGupta@users.noreply.github.com>
2020-11-18 13:20:36 -0500

    added SendEmailResult

---
+++ Naos.Email.Domain/Models/Enums/SendEmailResult.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SendEmailResult.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Email.Domain
+{
+    /// <summary>
+    /// The result of sending an email.
+    /// </summary>
+    public enum SendEmailResult
+    {
+        /// <summary>
+        /// Unknown (default).
+        /// </summary>
+        Unknown,
+
+        /// <summary>
+        /// The email was sent.
+        /// </summary>
+        Success,
+
+        /// <summary>
+        /// The email was not sent; failed to connect to the server.
+        /// </summary>
+        FailedToConnectToServer,
+
+        /// <summary>
+        /// The email was not sent; failed to authenticate with the server.
+        /// </summary>
+        FailedToAuthenticateWithServer,
+
+        /// <summary>
+        /// The email was not sent; a failure occurred when sending the email.
+        /// </summary>
+        FailedToSendEmailToServer,
+    }
+}
