5a3b30c4b694a9b4d81541d8368f3297795d483c
Suraj Gupta <SurajGupta@users.noreply.github.com>
2020-11-18 09:36:53 -0500

    added EmailOptions and EmailRequest

---
+++ Naos.Email.Domain/Models/Classes/EmailOptions.cs
@@ -0,0 +1,42 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="EmailOptions.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Email.Domain
+{
+    using System.Net.Mail;
+
+    using OBeautifulCode.Type;
+
+    /// <summary>
+    /// Some options/instructions about an email.
+    /// </summary>
+    public partial class EmailOptions : IModelViaCodeGen
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EmailOptions"/> class.
+        /// </summary>
+        /// <param name="priority">OPTIONAL value that specifies the priority of the email.  DEFAULT is to use the system default.</param>
+        /// <param name="deliveryNotification">OPTIONAL value that specifies the requested delivery notifications.  DEFAULT is to use the system default.</param>
+        public EmailOptions(
+            MailPriority? priority = null,
+            DeliveryNotificationOptions? deliveryNotification = null)
+
+        {
+            this.Priority = priority;
+            this.DeliveryNotification = deliveryNotification;
+        }
+
+        /// <summary>
+        /// Gets a value that specifies the priority of the email.
+        /// </summary>
+        public MailPriority? Priority { get; private set; }
+
+        /// <summary>
+        /// Gets a value that specifies the requested delivery notifications.
+        /// </summary>
+        public DeliveryNotificationOptions? DeliveryNotification { get; private set; }
+    }
+}
---
+++ Naos.Email.Domain/Models/Classes/EmailRequest.cs
@@ -0,0 +1,51 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="EmailRequest.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Email.Domain
+{
+    using OBeautifulCode.Assertion.Recipes;
+    using OBeautifulCode.Type;
+
+    /// <summary>
+    /// A request to send an email.
+    /// </summary>
+    public class EmailRequest : IModelViaCodeGen
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EmailRequest"/> class.
+        /// </summary>
+        /// <param name="validatedEmailAddresses">The email addresses to use in the email, where all of the email addresses have been validated.</param>
+        /// <param name="emailContent">The content of the email.</param>
+        /// <param name="emailOptions">OPTIONAL options/instructions for sending the email.  DEFAULT is to use the system defaults.</param>
+        public EmailRequest(
+            ValidatedEmailAddresses validatedEmailAddresses,
+            EmailContent emailContent,
+            EmailOptions emailOptions = null)
+        {
+            new { validatedEmailAddresses }.AsArg().Must().NotBeNull();
+            new { emailContent }.AsArg().Must().NotBeNull();
+
+            this.ValidatedEmailAddresses = validatedEmailAddresses;
+            this.EmailContent = emailContent;
+            this.EmailOptions = emailOptions;
+        }
+
+        /// <summary>
+        /// Gets the email addresses to use in the email, where all of the email addresses have been validated.
+        /// </summary>
+        public ValidatedEmailAddresses ValidatedEmailAddresses { get; private set; }
+
+        /// <summary>
+        /// Gets the content of the email.
+        /// </summary>
+        public EmailContent EmailContent { get; private set; }
+
+        /// <summary>
+        /// Gets options/instructions for sending the email.
+        /// </summary>
+        public EmailOptions EmailOptions { get; private set; }
+    }
+}
