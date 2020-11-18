69f8c7f5836ead59b886d23eea2fbf6fdcda27eb
Suraj Gupta <SurajGupta@users.noreply.github.com>
2020-11-18 13:09:33 -0500

    added EmailResponse and Email

---
+++ Naos.Email.Domain/Models/Classes/Email.cs
@@ -0,0 +1,43 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Email.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Email.Domain
+{
+    using OBeautifulCode.Assertion.Recipes;
+    using OBeautifulCode.Type;
+
+    /// <summary>
+    /// An email; the item itself along with information about its delivery.
+    /// </summary>
+    public class Email : IModelViaCodeGen
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Email"/> class.
+        /// </summary>
+        /// <param name="emailRequest">The request to send the email.</param>
+        /// <param name="emailResponse">The result of sending the email.</param>
+        public Email(
+            EmailRequest emailRequest,
+            EmailResponse emailResponse)
+        {
+            new { emailRequest }.AsArg().Must().NotBeNull();
+            new { emailResponse }.AsArg().Must().NotBeNull();
+
+            this.EmailRequest = emailRequest;
+            this.EmailResponse = emailResponse;
+        }
+
+        /// <summary>
+        /// Gets the request to send the email.
+        /// </summary>
+        public EmailRequest EmailRequest { get; private set; }
+
+        /// <summary>
+        /// Gets the result of sending the email.
+        /// </summary>
+        public EmailResponse EmailResponse { get; private set; }
+    }
+}
---
+++ Naos.Email.Domain/Models/Classes/EmailResponse.cs
@@ -0,0 +1,51 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="EmailResponse.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Email.Domain
+{
+    using System;
+
+    using OBeautifulCode.Assertion.Recipes;
+    using OBeautifulCode.Type;
+
+    using static System.FormattableString;
+
+    /// <summary>
+    /// The result of sending an email.
+    /// </summary>
+    public class EmailResponse : IModelViaCodeGen
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EmailResponse"/> class.
+        /// </summary>
+        /// <param name="sendEmailResult">The result of sending the email.</param>
+        /// <param name="exceptionToString">If there's a failure, the <see cref="object.ToString()"/> of the <see cref="Exception"/>.</param>
+        public EmailResponse(
+            SendEmailResult sendEmailResult,
+            string exceptionToString)
+        {
+            new { sendEmailResult }.AsArg().Must().NotBeEqualTo(SendEmailResult.Unknown);
+
+            if (sendEmailResult != SendEmailResult.Success)
+            {
+                new { exceptionToString }.AsArg().Must().NotBeNullNorWhiteSpace(Invariant($"When {nameof(sendEmailResult)} indicates a failure, {nameof(exceptionToString)} is required.");)
+            }
+
+            this.SendEmailResult = sendEmailResult;
+            this.ExceptionToString = exceptionToString;
+        }
+
+        /// <summary>
+        /// Gets the result of sending the email.
+        /// </summary>
+        public SendEmailResult SendEmailResult { get; private set; }
+
+        /// <summary>
+        /// Gets the <see cref="object.ToString()"/> of the <see cref="Exception"/> for failures.
+        /// </summary>
+        public string ExceptionToString { get; private set; }
+    }
+}
