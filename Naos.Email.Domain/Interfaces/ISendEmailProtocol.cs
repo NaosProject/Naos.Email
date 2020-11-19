043224f8c7413e478eaec2bc4b984fb370343610
Suraj Gupta <SurajGupta@users.noreply.github.com>
2020-11-19 09:27:48 -0500

    Added ISendEmailProtocol

---
+++ Naos.Email.Domain/Interfaces/ISendEmailProtocol.cs
@@ -0,0 +1,17 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ISendEmailProtocol.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Email.Domain
+{
+    using Naos.Protocol.Domain;
+
+    /// <summary>
+    /// Executes a <see cref="SendEmailOp"/>.
+    /// </summary>
+    public interface ISendEmailProtocol : ISyncAndAsyncReturningProtocol<SendEmailOp, EmailResponse>
+    {
+    }
+}
