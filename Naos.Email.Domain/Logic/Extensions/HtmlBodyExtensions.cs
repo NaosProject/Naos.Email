df2f17d37bb8164a593f586dabd79ea610cb0dd4
Suraj Gupta <SurajGupta@users.noreply.github.com>
2020-11-19 13:17:31 -0500

    added HtmlBodyExtensions

---
+++ Naos.Email.Domain/Logic/Extensions/HtmlBodyExtensions.cs
@@ -0,0 +1,34 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="HtmlBodyExtensions.cs" company="Naos Project">
+//    Copyright (c) Naos Project 2019. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Naos.Email.Domain
+{
+    using OBeautifulCode.Assertion.Recipes;
+
+    /// <summary>
+    /// Extension methods on <see cref="string"/> HTML bodies.
+    /// </summary>
+    public static class HtmlBodyExtensions
+    {
+        /// <summary>
+        /// Wraps the specified preview text in a div tag with the correct styling
+        /// to have it interpreted as preview text by email clients.
+        /// </summary>
+        /// <param name="previewText">The preview text.</param>
+        /// <returns>
+        /// The div tag to use.
+        /// </returns>
+        public static string ToPreviewTextHtmlDiv(
+            this string previewText)
+        {
+            new { previewText }.AsArg().Must().NotBeNullNorWhiteSpace();
+
+            var result = $@"<div style=""display: none; max-height: 0px; overflow: hidden;"">{previewText}</div>";
+
+            return result;
+        }
+    }
+}
